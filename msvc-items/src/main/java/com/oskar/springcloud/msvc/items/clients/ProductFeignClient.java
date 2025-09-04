ae96ab2d973348248ddea7e26e4c0351743392b9
kok4depelover <oskarcermeno1@gmail.com>
2025-09-03 23:42:03 -0600

    Add msvc

--- msvc-items/src/main/java/com/oskar/springcloud/msvc/items/clients/ProductFeignClient.java
+++ msvc-items/src/main/java/com/oskar/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.oskar.springcloud.msvc.items.models.Product;
 
 
-@FeignClient(url="localhost:8001")
-public class ProductsFeignClient {
-    
+@FeignClient(name="msvc-products")
+public interface ProductFeignClient {
+
     @GetMapping
     List<Product> findAll();
 
+    @GetMapping("/{id}")
+    Product details(@PathVariable Long id);
 }
